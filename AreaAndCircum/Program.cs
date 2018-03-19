04ba0f271e4d883f6d15013246f0d9565fbf5506
Joanne <thecodingdiva@gmail.com>
2018-03-19 18:39:00 -0400

    Add project files.

---
+++ AreaAndCircum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaAndCircum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+            Console.WriteLine("Enter the radius: ");
+            double radius = Console.Read();
+
+            double circumference = 2 * pi * radius;
+            double area = pi * (radius * radius);
+
+            Console.WriteLine("Circumference = " + circumference + "/nArea = " + area);
+
+
+        }
+    }
+}
