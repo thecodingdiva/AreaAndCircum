03cd63485ccb9b02b14b690ada67f0e055aefc29
Joanne <thecodingdiva@gmail.com>
2018-03-20 10:09:56 -0400

    Made pi a constant

--- AreaAndCircum/Program.cs
+++ AreaAndCircum/Program.cs
@@ -10,14 +10,14 @@ namespace AreaAndCircum
     {
         static void Main(string[] args)
         {
-            double pi = 3.14159;
+            const double PI = 3.14159;
             Console.WriteLine("Enter the radius: ");
             String r = Console.ReadLine();
             double radius = Convert.ToDouble(r);
 
-            double circumference = 2 * pi * radius;
-            double area = pi * (radius * radius);
-            double volume = (((4 / 3) * pi * (radius * radius * radius)) / 2);
+            double circumference = 2 * PI * radius;
+            double area = PI * (radius * radius);
+            double volume = (((4 / 3) * PI * (radius * radius * radius)) / 2);
 
             Console.WriteLine("Circumference = " + circumference + "\nArea = " + area + "\nVolume = " + volume);
         }
