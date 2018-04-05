a19e79dd4a0d60efc8b7d285e1675020d85ce5fe
Joanne <thecodingdiva@gmail.com>
2018-04-05 13:52:06 -0400

    Added exception handling

--- AreaAndCircum/Program.cs
+++ AreaAndCircum/Program.cs
@@ -10,16 +10,82 @@ namespace AreaAndCircum
     {
         static void Main(string[] args)
         {
-            const double PI = 3.14159;
+            try
+            {
+                Console.WriteLine("What action would you like to perform: \n1) Circumference of a Circle \n2) Area of a Circle \n3) Volume of a hemisphere: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                if (option > 3 || option < 0)
+                {
+                    throw new FormatException("Number not in range.");
+                }
+                else
+                {
+                    double radius = GetRadius();
+
+                    if (radius == 0)
+                    {
+                        throw new Exception("Number cannot be 0");
+                    }
+                    else
+                    {
+                        switch (option)
+                        {
+                            case 1:
+                                CircleCircumfrence(radius);
+                                break;
+
+                            case 2:
+                                CircleArea(radius);
+                                break;
+
+                            case 3:
+                                HemisphereVolume(radius);
+                                break;
+                        }
+                    }                    
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Your input is invalid");
+            }
+            catch (Exception e1)
+            {
+                Console.WriteLine("Input cannot be 0");
+            }
+        }
+
+        static double GetRadius()
+        {
             Console.WriteLine("Enter the radius: ");
             String r = Console.ReadLine();
             double radius = Convert.ToDouble(r);
+            return radius;
+        }
 
+        static double CircleCircumfrence(double radius)
+        {
+            const double PI = 3.14159;
             double circumference = 2 * PI * radius;
-            double area = PI * (radius * radius);
-            double volume = (((4 / 3) * PI * (radius * radius * radius)) / 2);
+            Console.WriteLine("The Circumfrence is: " + circumference);
+            return circumference;
+        }
 
-            Console.WriteLine("Circumference = " + circumference + "\nArea = " + area + "\nVolume = " + volume);
+        static double CircleArea(double radius)
+        {
+            const double PI = 3.14159;
+            double area = PI * (radius * radius);
+            Console.WriteLine("The Area is: " + area);
+            return area;
+        }
+
+        static double HemisphereVolume(double radius)
+        {
+            const double PI = 3.14159;
+            double volume = (((4 / 3) * PI * (radius * radius * radius)) / 2);
+            Console.WriteLine("The Volume is: " + volume);
+            return volume;
         }
     }
 }
